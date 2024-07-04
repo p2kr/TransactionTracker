c157558583da338cc6e0c303352d21f05b317dc9
p2kr <34896515+p2kr@users.noreply.github.com>
2024-07-04 19:44:11 +0530

    Reducing cognitive complexity of connector

--- Utils/GmailConnector2.cs
+++ Utils/GmailConnector2.cs
@@ -1,90 +1,117 @@
 ﻿using Google.Apis.Auth.OAuth2;
 using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
 using Google.Apis.Util.Store;
+using System.Diagnostics;
 using System.Text;
 
 namespace TransactionTracker.Utils
 {
-    public static class GmailConnector2
-    {
-        private static readonly ILogger LOG = CustomLogger.GetLogger(typeof(GmailConnector2));
+	public static class GmailConnector2
+	{
+		private static readonly ILogger Log = CustomLogger.GetLogger(typeof(GmailConnector2));
 
-        private static readonly string CLIENT_SECRETS = "Assets/client_secrets.json";
-        public static async Task<List<string>> ConnectToGmail()
-        {
-            List<string> listOfMessages = [];
+		/// <summary>
+		/// Connect to Gmail service and fetch emails
+		/// </summary>
+		/// <returns>List of email text in html format</returns>
+		public static async Task<List<string>> ConnectToGmail(ConnectorConfig config)
+		{
+			var watch = Stopwatch.StartNew();
+			List<string> listOfMessages = [];
 
-            UserCredential credential;
-            using (var stream = new FileStream(CLIENT_SECRETS, FileMode.Open, FileAccess.Read))
-            {
-                credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
-                    (await GoogleClientSecrets.FromStreamAsync(stream)).Secrets,
-                    [GmailService.Scope.GmailReadonly],
-                    "user",
-                    CancellationToken.None,
-                    new FileDataStore("Assets/Emails.UserInbox")
-                    );
-            }
+			UserCredential credential = await GetCredential(config);
 
-            // Create the service
-            var service = new GmailService(new GmailService.Initializer()
-            {
-                HttpClientInitializer = credential,
-                ApplicationName = "TransationTracker",
-            });
+			// Create the service
+			var service = new GmailService(new GmailService.Initializer()
+			{
+				HttpClientInitializer = credential,
+				ApplicationName = "TransationTracker",
+			});
 
-            var inboxListRequest = service.Users.Messages.List("me");
-            inboxListRequest.LabelIds = "INBOX";
-            inboxListRequest.IncludeSpamTrash = true;
+			var inboxListRequest = service.Users.Messages.List("me");
+			BuildMessagesRequest(inboxListRequest, config);
 
-            var msgList = await inboxListRequest.ExecuteAsync();
+			var msgList = await inboxListRequest.ExecuteAsync();
 
-            if (msgList != null && msgList.Messages != null)
-            {
-                foreach (var msg in msgList.Messages)
-                {
-                    try
-                    {
+			if (msgList != null && msgList.Messages != null)
+			{
+				foreach (var msg in msgList.Messages)
+				{
+					if (msg != null)
+					{
+						await GetAllMessages(listOfMessages, service, msg);
+					}
+				}
+				Log.LogInformation("Found {N} messages in {Timespan}", listOfMessages.Count, watch.Elapsed);
+			}
+			return listOfMessages;
+		}
 
-                        var fullMessageRequest = service.Users.Messages.Get("me", msg.Id);
-                        var fullMessageResponse = await fullMessageRequest.ExecuteAsync();
-                        var msgParts = fullMessageResponse.Payload;
+		private static async Task GetAllMessages(List<string> listOfMessages, GmailService service, Message msg)
+		{
+			try
+			{
+				var fullMessageRequest = service.Users.Messages.Get("me", msg.Id);
+				var fullMessageResponse = await fullMessageRequest.ExecuteAsync();
+				var msgParts = fullMessageResponse.Payload;
 
-                        StringBuilder message = new("");
+				StringBuilder message = new("");
 
-                        if (msgParts.Parts != null && msgParts.Parts.Any())
-                        {
-                            foreach (var part in msgParts.Parts)
-                            {
-                                if (part?.Body?.Data != null)
-                                {
-                                    message.Append(Encoding.UTF8.GetString(FromBase64ForUrlString(part.Body.Data)));
-                                }
-                            }
-                        }
-                        else if (msgParts.Body != null && msgParts.Body.Data.Length != 0)
-                        {
-                            message.Append(Encoding.UTF8.GetString(FromBase64ForUrlString(msgParts.Body.Data)));
-                        }
+				if (msgParts.Parts != null && msgParts.Parts.Any())
+				{
+					foreach (var part in msgParts.Parts)
+					{
+						if (part?.Body?.Data != null)
+						{
+							message.Append(Encoding.UTF8.GetString(FromBase64ForUrlString(part.Body.Data)));
+						}
+					}
+				}
+				else if (msgParts.Body != null && msgParts.Body.Data.Length != 0)
+				{
+					message.Append(Encoding.UTF8.GetString(FromBase64ForUrlString(msgParts.Body.Data)));
+				}
 
-                        listOfMessages.Add(message.ToString());
-                    }
-                    catch (Exception ex)
-                    {
-                        LOG.LogError(ex, "Error in {Snippet}", msg.Snippet);
-                    }
-                }
-            }
-            return listOfMessages;
-        }
-        public static byte[] FromBase64ForUrlString(string base64ForUrlInput)
-        {
-            int padChars = (base64ForUrlInput.Length % 4) == 0 ? 0 : (4 - (base64ForUrlInput.Length % 4));
-            StringBuilder result = new(base64ForUrlInput, base64ForUrlInput.Length + padChars);
-            result.Append(String.Empty.PadRight(padChars, '='));
-            result.Replace('-', '+');
-            result.Replace('_', '/');
-            return Convert.FromBase64String(result.ToString());
-        }
-    }
+				listOfMessages.Add(message.ToString());
+			}
+			catch (Exception ex)
+			{
+				Log.LogError(ex, "Error in {Snippet}", msg.Snippet);
+			}
+		}
+
+		private static async Task<UserCredential> GetCredential(ConnectorConfig config)
+		{
+			UserCredential credential;
+			using (var stream = new FileStream(config.ClientSecretsLocation, FileMode.Open, FileAccess.Read))
+			{
+				credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+					(await GoogleClientSecrets.FromStreamAsync(stream)).Secrets,
+					[GmailService.Scope.GmailReadonly],
+					"user",
+					CancellationToken.None,
+					new FileDataStore(config.CredentialsDatastore)
+					);
+			}
+			return credential;
+		}
+
+		private static void BuildMessagesRequest(UsersResource.MessagesResource.ListRequest msgRequest, ConnectorConfig config)
+		{
+			msgRequest.LabelIds = config.Labels;
+			msgRequest.IncludeSpamTrash = config.IncludeSpamTrash;
+			msgRequest.MaxResults = config.MaxResults;
+		}
+
+		public static byte[] FromBase64ForUrlString(string base64ForUrlInput)
+		{
+			int padChars = (base64ForUrlInput.Length % 4) == 0 ? 0 : (4 - (base64ForUrlInput.Length % 4));
+			StringBuilder result = new(base64ForUrlInput, base64ForUrlInput.Length + padChars);
+			result.Append(String.Empty.PadRight(padChars, '='));
+			result.Replace('-', '+');
+			result.Replace('_', '/');
+			return Convert.FromBase64String(result.ToString());
+		}
+	}
 }
